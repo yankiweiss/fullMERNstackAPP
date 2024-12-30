b5a52a1cb0b426c0bfaf2809896d34f2635269fd
yankiweiss <yakovw2706@gmail.com>
2024-12-30 16:52:03 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
+console.log(process.env.MONGO_URI);
+
+
+app.get('/', (req, res)=>{
+    res.send('Server is Ready!')
+})
+
 app.listen(5000, ()=> {
-    console.log("Server is runing on 5000")
+    connectDB()
+    console.log("Server is runing on 5000 hello hi")
 })
